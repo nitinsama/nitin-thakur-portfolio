2915ddd0eaaf94667c09d28388587b51684f476e
Nitin Thakur <nitin21099@gmail.com>
2021-05-30 20:05:15 +0530

    changes

--- src/linkPages.js
+++ src/linkPages.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom";
 import "./linkPages.css"
 
+
 class LinkPages extends React.Component{
     render(){
         return(
@@ -10,12 +11,30 @@ class LinkPages extends React.Component{
                 <div className="container-fluid projectContainer">
                 <div className="row projectRow">
                     <div className="col projectCol">
-                        <Link to="/shopping"><button class="btn button1"><h2>Buy Cloths</h2></button></Link>
-                        <Link to="/cityTour"><button class="btn button2"><h2>City Tour</h2></button></Link>
+                        <Link to="/shopping"><button class="btn button1" 
+                        style={{ backgroundImage: "url(/images/cloths.jpeg)" ,
+                                backgroundSize: "100%",
+                                opacity: "50%"
+                                }} >
+                                <h2>Buy Cloths</h2></button></Link>
+                        <Link to="/cityTour"><button class="btn button2"
+                        style={{ backgroundImage: "url(/images/tour.jpg)" ,
+                        backgroundSize: "100%",
+                        opacity: "50%"
+                        }}><h2>City Tour</h2></button></Link>
                     </div>
                     <div className="col projectCol">
-                        <Link to="/todoList"><button class="btn button3"><h2>To-Do List</h2></button></Link>
-                        <Link to="/recipeFinder"><button class="btn button4"><h2>Recipe Finder</h2></button></Link>
+                        <Link to="/todoList"><button class="btn button3"
+                        style={{ backgroundImage: "url(/images/todo.jpg)" ,
+                        backgroundSize: "100%",
+                        opacity: "50%"
+                        }}
+                        ><h2>To-Do List</h2></button></Link>
+                        <Link to="/recipeFinder">
+                        <button class="btn button4" style={{ backgroundImage: "url(/images/recipe.jpg)" ,
+                                backgroundSize: "100%",
+                                opacity: "50%"
+                                }}><h2>Recipe Finder</h2></button></Link>
                     </div>
                 </div>
             </div>
